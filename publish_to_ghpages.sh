35c75798d1ad8b0097d58ea5a2f50e4e8460c907
Tengyuan Liang <Tengyuan.Liang@chicagobooth.edu>
2019-09-11 19:28:48 -0500

    Update publish_to_ghpages.sh

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -16,11 +16,11 @@ rm -rf .git/worktrees/public/
 echo "Checking out gh-pages branch into public"
 git worktree add -B gh-pages public origin/gh-pages
 
-# echo "Removing existing files"
-# rm -rf public/*
+echo "Removing existing files"
+rm -rf public/*
 
-# echo "Generating site"
-# hugo
+echo "Generating site"
+hugo
 
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
